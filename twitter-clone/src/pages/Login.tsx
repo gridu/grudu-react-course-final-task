55f59b540440aee7dfe634b6f5aa7e3eced7d067
danielrfranco <danielfranco9132@gmail.com>
2022-06-04 10:46:15 -0500

    Start building router

---
+++ twitter-clone/src/pages/Login.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div className="login">
+      <div className='login-container'>
+        <h1>Login</h1>
+        
+      </div>
+    </div>
+  )
+};
+
+export default Login;
