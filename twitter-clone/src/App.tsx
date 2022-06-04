af917c96e01eaf87794e077aaebddd850f23ba6a
danielrfranco <danielfranco9132@gmail.com>
2022-06-04 14:03:45 -0500

    Build signup page

--- twitter-clone/src/App.tsx
+++ twitter-clone/src/App.tsx
@@ -3,12 +3,14 @@ import { Routes, Route, Link } from "react-router-dom";
 import logo from './logo.svg';
 
 import Login from "./pages/Auth/Login";
+import Signup from './pages/Auth/Signup';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/sign-up" element={<Signup />} />
       </Routes>
     </div>
   );
--- twitter-clone/src/pages/Auth/Signup.tsx
+++ twitter-clone/src/pages/Auth/Signup.tsx
@@ -1,5 +1,88 @@
-import React from "react";
+import React from 'react';
+import styled from '@emotion/styled';
+import { Formik, Form, ErrorMessage } from 'formik';
+import { Link, useNavigate } from 'react-router-dom';
+import * as Yup from 'yup';
 
-const Signup = () => null;
+import LoginWrapper from './components/LoginWrapper';
+import LoginContainer from './components/LoginContainer';
+import LoginBox from './components/LoginBox';
+import LoginFooterLink from './components/LoginFooterLink';
+import Field from '../../components/Input';
+import Button from '../../components/Button';
+
+const StyledForm = styled(Form)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Signup = () => {
+  const navigate = useNavigate();
+
+  return (
+    <LoginWrapper>
+      <LoginContainer>
+        <LoginBox>
+          <h1>Sign Up</h1>
+
+          <Formik
+            initialValues={{ email: '',  password: '', username: '', fullname: '' }}
+            validationSchema={Yup.object({
+              email: Yup.string()
+                .email('Please use email format'),
+              fullname: Yup.string()
+                .min(15, 'Must be 15 characters at least')
+                .required('Required'),
+              username: Yup.string()
+                .required('Required'),
+              password: Yup.string()
+                .min(8, 'Must be 8 characters at least')
+                .required('Required'),
+            })}
+            onSubmit={(values, { setSubmitting }) => {
+              setSubmitting(true);
+              fetch(`http://localhost:3001/users/${values.username}`).then((res) => {
+                console.log(res.status, res.statusText);
+                if (res.status === 200) {
+                  navigate('/home', {replace: true});
+                }
+              }).catch((e) => {
+                console.error(e)
+              }).finally(() => {
+                setSubmitting(false);
+              })
+            }}
+          >
+            {({ isSubmitting }) => (
+              <StyledForm>
+                <Field type="email" name="email" placeholder='Email'/>
+                <ErrorMessage name="email" component="div" />
+
+                <Field type="password" name="password" placeholder='Password' />
+                <ErrorMessage name="password" component="div" />
+
+                <Field type="text" name="username" placeholder='Username'/>
+                <ErrorMessage name="username" component="div" />
+
+                <Field type="text" name="fullname" placeholder='Full name'/>
+                <ErrorMessage name="fullname" component="div" />
+
+                <Button align='flex-end' type="submit" disabled={isSubmitting}>
+                  Sign up
+                </Button>
+              </StyledForm>
+            )}
+          </Formik>
+
+        </LoginBox>
+
+        <LoginFooterLink>
+          Already have an account? <Link to="/">Log in</Link>
+        </LoginFooterLink>
+      </LoginContainer>
+    </LoginWrapper>
+  )
+};
 
 export default Signup;
